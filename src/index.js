a8fa266b7ff40b27eb12e924eb44b4e233c96852
Marlene Angouillant <marlene.angouillant@gmail.com>
2019-03-28 15:13:45 +0100

    quest React 2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Travel from "./Travel";
-
-const InfoTravels = {
-    destination: "New-York",
-    country: "Etats-Unis",
-    photo: "http://getwallpapers.com/wallpaper/full/2/c/5/619418.jpg",
-    distance: "5785 km"
-};
-
-const InfoTravels2 = {
-    destination: "Corse",
-    country: "France",
-    photo: "https://www.kuoni.fr/wp-content/uploads/2017/12/Corse-Croisiere-iStock-586199376_sam74100.jpg",
-    distance: "1099 km"
-};
-
-const App = () => (
-    <div>
-        <Travel {...InfoTravels} />
-        <Travel {...InfoTravels2} />
-    </div>
-);
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
